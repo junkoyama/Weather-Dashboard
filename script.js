4405626b11c09fbc439e92a71b8f45052d60bddc
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2020-12-14 22:19:00 -0500

    Added date using moment.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var APIKey = "6d2b6b9cf67e7c416ba9a947cbfcd77e";
   var queryURL;
-  
+  var today = moment(new Date()).format("MM/DD/YYYY");
+
   function getCurrentCondition(url, data) {
-      console.log(url);
-      console.log(data);
-  
-      // Transfer content to HTML
-      $(".city").html(`<h1> ${data.name} Weather Details </h1>`);
-      $(".wind").text(`Wind Speed: ${data.wind.speed}`);
-      $(".humidity").text(`Humidity: ${data.main.humidity}`);
-  
-      // Convert the temp to fahrenheit
-      var tempF = (data.main.temp - 273.15) * 1.8 + 32;
-  
-      // add temp content to html
-      $(".tempF").text(`Temperature (F) ${tempF.toFixed(2)}`);
-  
-      // Log the data in the console as well
-      console.log(`Wind Speed: ${data.wind.speed}`);
-      console.log(`Humidity: ${data.main.humidity}`);
-      console.log(`Temperature (F): ${tempF}`);
-  };
-  
+    console.log(url);
+    console.log(data);
+
+    // Transfer content to HTML
+    $(".city").html(`<h1> ${data.name} Weather Details for ${today} </h1>`);
+    $(".wind").text(`Wind Speed: ${data.wind.speed}`);
+    $(".humidity").text(`Humidity: ${data.main.humidity}`);
+
+    // Convert the temp to fahrenheit
+    var tempF = (data.main.temp - 273.15) * 1.8 + 32;
+
+    // add temp content to html
+    $(".tempF").text(`Temperature (F) ${tempF.toFixed(2)}`);
+
+    // Log the data in the console as well
+    console.log(`Wind Speed: ${data.wind.speed}`);
+    console.log(`Humidity: ${data.main.humidity}`);
+    console.log(`Temperature (F): ${tempF}`);
+  }
+
   $("#search-button").on("click", function () {
     var inputField = $("#search-value").val();
     queryURL =
@@ -33,8 +34,8 @@ $(document).ready(function() {
       APIKey;
     console.log(inputField);
 
-   $(".search-history").append(`<p> ${inputField} </p>`);
-   
+    $(".search-history").append(`<p> ${inputField} </p>`);
+
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
       url: queryURL,
@@ -42,9 +43,7 @@ $(document).ready(function() {
     })
       // We store all of the retrieved data inside of an object called "response"
       .then(function (response) {
-          getCurrentCondition(queryURL, response);
-  
+        getCurrentCondition(queryURL, response);
       });
   });
-  
 });
