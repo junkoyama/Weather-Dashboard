fec0c5f9773070c5825970162448deb415e6b6c7
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2020-12-19 15:23:07 -0500

    Added cards for 5 day forecast; hid the cards when page first loads from script.js)

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 $(document).ready(function () {
+  $(".card").hide();
   var APIKey = "6d2b6b9cf67e7c416ba9a947cbfcd77e";
   var queryURL;
   var weatherIconUrl;
+  // var UV_index;
+
+  //Set variable for today's date from moment.js
   var todayDate = moment(new Date()).format("MM/DD/YYYY");
 
   function getCurrentCondition(url, data) {
-    console.log(url);
-    console.log(data);
+    var getCityName = data.name;
     var weatherIcon = data.weather[0].icon;
+    var getLatitude = data.coord.lat;
+    var getLongitude = data.coord.lon;
+    var getWindSpeed = data.wind.speed;
+    var getHumidity = data.main.humidity;
+    console.log(getLongitude + ", " + getLatitude);
 
+    // Adds weather icon next to city name weather details
     weatherIconUrl = "http://openweathermap.org/img/wn/" +
     weatherIcon +
-    ".png"
+    ".png";
+
+    // UV_index = "http://api.openweathermap.org/data/2.5/uvi?lat="
+    // + getLatitude +
+    // "&lon=" +
+    // getLongitude +
+    // "&appid=" +
+    // APIKey;
     
-    // Transfer content to HTML
-    $("#today").html(`<h1> ${data.name} Weather Details for ${todayDate} <img class="weather-icon" src="icons/unknown.png"/> </h1>`);
-    $(".wind").text(`Wind Speed: ${data.wind.speed}`);
-    $(".humidity").text(`Humidity: ${data.main.humidity}`);
+    // Adds text to the HTMl div class tags
+    $("#today").html(`<h1> ${getCityName} Weather Details for ${todayDate} <img class="weather-icon" src="icons/unknown.png"/> </h1>`);
+    $(".wind").text(`Wind Speed: ${getWindSpeed}`);
+    $(".humidity").text(`Humidity: ${getHumidity}`);
     $(".weather-icon").attr('src', weatherIconUrl);
+    // $(".UVindex").text(`UV Index: ${UV_index}`);
 
-    // Convert the temp to fahrenheit
+    // Below converts the temp to fahrenheit
     var tempF = (data.main.temp - 273.15) * 1.8 + 32;
-
-    // add temp content to html
     $(".tempF").text(`Temperature (F) ${tempF.toFixed(2)}`);
-
-    // Log the data in the console as well
-    console.log(`Wind Speed: ${data.wind.speed}`);
-    console.log(`Humidity: ${data.main.humidity}`);
-    console.log(`Temperature (F): ${tempF}`);
   }
-
+  // Add event listener for search button click
   $("#search-button").on("click", function () {
     var inputField = $("#search-value").val();
     queryURL =
@@ -39,8 +49,8 @@ $(document).ready(function () {
       inputField +
       "&appid=" +
       APIKey;
-    console.log(inputField);
 
+    // Append the search history input value below the box
     $(".search-history").append(`<p> ${inputField} </p>`);
 
     // Here we run our AJAX call to the OpenWeatherMap API
